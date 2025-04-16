a0cee9b4e1df687a648c37d8d7132b61e865a77b
lumens <luisfelixnogueira@gmail.com>
2025-04-16 20:42:07 -0300

    Subindo projeto criado com o Start Spring

---
+++ src/test/java/com/lumens/DIO_CHALLENGE/DioChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lumens.DIO_CHALLENGE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DioChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
